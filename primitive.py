1e96a4b0e0385ad22367eb5c3e516c2c9806d06c
Alex <theeduazizjon@gmail.com>
2026-04-16 14:27:38 +0900

    feat:learned string in python

--- primitive.py
+++ primitive.py
@@ -15,3 +15,21 @@ print(f"the count: {count} and the type of variable is{count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+print("=====string=====")
+# METHODS: upper(), lower(), title(), find(), replace()
+
+course = "AI Python Fullstack"
+result_1 = type(course)
+
+print(f"the type of course: {result_1}")
+
+result_2 = course.title()
+print(f"the result:{result_2}")
+
+result_3 = course.upper()
+print(f"the result:{result_3}")
+
+result_4 = course.replace("FullStack", "MasterClass")
+print(f"the result: {result_4}")
+print(course)
