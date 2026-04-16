0d294a6ed3e00fa06656cc7b8b6a62eaa5e173fd
Alex <theeduazizjon@gmail.com>
2026-04-16 13:52:26 +0900

    BRR:Initial Setup

---
+++ run.py
@@ -0,0 +1,16 @@
+# Dunder - double underlines:  __builtins__ , __init__
+
+# example_1
+message = "Everything in pyhton is an object"
+print(message)
+
+result = type(message)
+print("result:", result)
+
+''' 
+In Pyhton , there are builtin tools:
+1 - TYPES > int float str list dict 
+2 - FUNCTION > print() , len(), input(), type()
+3 - CONSTANTS > True or False None 
+'''
+print(dir(__builtins__))
