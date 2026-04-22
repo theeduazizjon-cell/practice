48e9b6eb94ab1184e2b9bad5d42f8e8f00a72499
Alex <theeduazizjon@gmail.com>
2026-04-22 14:37:02 +0900

    fix: Worked on a new algorithmic problem

--- train.js
+++ train.js
@@ -1,9 +1,32 @@
+/* 
+C - Task
+Savol:
+Shunday function tuzing, u 2 ta string parametr ga ega bolsin, hamda 
+agar har ikkala string bir hil harfladan iborat bolsa true, aks holda 
+false qaytarsin 
+Masalan: checkContent("mitgroup","gmtiptou") 
+true ni return qiladi 
+*/
+
+// Masalaning yechimi 
+
+function checkContent(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    let sortedstr1 = str1.split('').sort('').join('');
+    let sortedstr2 = str2.split('').sort('').join('')
+
+    return sortedstr1 == sortedstr2;
+}
+
+console.log(checkContent("mitgroup", ("gmtiprou"))
 /* B - Task 
 Savol:
 Shunday function tuzing, u 1 ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin. 
 MASALAN: countDigits("ad2a54y79wet0sfgb9")
 7 ni return qiladi 
-*/
 
 function countDigits(str) {
     let count = 0;
@@ -16,9 +39,7 @@ function countDigits(str) {
     return count;
 }
 console.log(countDigits("ad2a54y79wet0sfgb9"));
-
-
-
+*/
 
 /* A - Task:
  Savol: Shunday 2 parametrli function tuzing , 
