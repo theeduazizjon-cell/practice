3371c4a62a96cdc5b5fa8ef8753330a622f981df
Alex <theeduazizjon@gmail.com>
2026-04-24 11:28:33 +0900

    feat: learned zip object

--- tuples.py
+++ tuples.py
@@ -81,3 +81,13 @@ def greeting(*args, **kwargs):
 
 # CALL
 greeting("Hi", True, 10, name="John", age=22)
+
+
+print("==== ZIP ====")
+tuple1 = (1, 2, 3, 4)
+tuple2 = ("a", "b", "c")
+
+zipped = zip(tuple1, tuple2)
+print("zipped:", zipped)
+result = list(zipped)
+print(f"the result: {result}")
