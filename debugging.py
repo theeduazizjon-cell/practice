72f0f4235db0c844583d564dd2816226bf1ac863
Alex <theeduazizjon@gmail.com>
2026-04-29 12:30:24 +0900

    feat: Core package turtle simple example

---
+++ debugging.py
@@ -0,0 +1,20 @@
+"""
+Package and Debugging 
+1) Python Packages & Core Package 
+2) Package manager & external package 
+3) Debugging 
+"""
+
+import turtle
+print("=== Python pakcages & Core packages ===")
+# Python Packages/Modules: Core, File and External
+
+# Core pakcages > https://docs.python.org/3/library
+
+
+t = turtle.Turtle()
+t.shape("turtle")
+t.speed(1)
+t.circle(100)
+
+turtle.done()
