c09469978f4f422fcade80bdcffa832f273acfab
Alex <theeduazizjon@gmail.com>
2026-05-11 19:03:56 +0900

    feat: New algorithmic problem in Python

--- train.py
+++ train.py
@@ -1,4 +1,30 @@
 """
+K - Task 
+
+Masalaning sharti:
+
+Shunday function yozing, u string qabul qilsin 
+va string ichidagi eng uzun sozni qaytarsin.
+
+MASALAN: find_longest("I come from Uzbekistan") 
+return "Uzbekistan"
+
+Masalaning yechimi:
+
+"""
+
+
+def find_longest(text):
+    words = text.split()
+    longest_word = ""
+    for word in words:
+        if len(word) > len(longest_word):
+            longest_word = word
+    return longest_word
+
+
+print(find_longest("I come from Uzbekistan"))
+"""
 I - Task
 
 Masalaning  sharti: Shunday function tuzing, unga string argument pass bolsin.
